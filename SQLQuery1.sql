ea7b7a329bd4e083390cd0a89132e825d512e8b4
ossained <ossained22k7@gmail.com>
2025-03-25 09:15:17 +0000

    updated

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -53,7 +53,9 @@ cross join allrows)
  and AcceptedCmp3= 0 and AcceptedCmp4=0 and AcceptedCmp5= 0
  
  
-  create view most_Accepted as
+ create view most_Accepted as
+  
+  
   with cte as (
   select id,case when AcceptedCmp1 =1 and AcceptedCmp6=1 then ID else 0 end as comp1,
 	     case when AcceptedCmp2 =1 and AcceptedCmp6=1 then ID else 0 end as comp2,
@@ -77,16 +79,224 @@ cross join allrows)
 
 select id
   from figure2
-  where figure>2
+  where figure>=2
 
 
-  with cte as(
-  select id from marketing_data
-  where AcceptedCmp6 =1 and id in ( select * from most_Accepted))
 
   --checking for % of customers that accepted more than 2 comp and accepted comp6
   select count(id)* 100.0 / (select count (id) from marketing_data where AcceptedCmp6 = 1)
   from most_Accepted
   
+select * from marketing_data
   
+  -- Test group: Customers who accepted campaign 6 but not enough other campaigns
+
+SELECT id,sum(Income),Education,Marital_Status,Country--appears to have low income
+FROM marketing_data
+WHERE AcceptedCmp6 = 1
+  AND (AcceptedCmp1*1.0 + AcceptedCmp2*1.0 + AcceptedCmp3*1.0 + AcceptedCmp4*1.0 + AcceptedCmp5*1.0 + AcceptedCmp6*1.0) <= 2 
+
+
+
+   --checking for % of customers that accepted more than 2 comp and accepted comp6
+  select count(id)* 100.0 / (select count (id) from marketing_data where AcceptedCmp6 = 1)
+  from most_Accepted
+
+
   
+select count(*)* 100.0 / (select count(*)  from most_Accepted )
+  from  marketing_data ma 
+where Recency <= 30 and id in (select id from most_Accepted)
+---percentage of people that made orders in the past 30days
+with total_no as (
+select count(*) as cnt from most_Accepted mo
+left join marketing_data  md on md.ID =mo.id
+where Recency <= 30 and md.ID in (select md.ID from most_Accepted)),
+
+no_accepted as (
+select count(*) as cnt1 from most_Accepted)
+
+select cnt *100.0 /  cnt1 
+from total_no
+cross join no_accepted
+
+
+
+SELECT ma.id,Education,Marital_Status,
+sum(Income)as total_income,  sum(NumDealsPurchases) as total_discount,
+       sum(numwebpurchases) as total_webpurchase,
+	   sum(NumCatalogPurchases) as total_catelog,
+	   sum(NumStorePurchases) as total_storepurchase,
+	   sum(NumWebVisitsMonth) as total_webvisit from most_Accepted ma
+left join marketing_data  md  on ma.id = md.id
+ group by ma.id,Education,Marital_Status
+ order by total_income desc
+
+ select sum(NumWebVisitsMonth)as cnt,sum(Income)as income,sum(NumStorePurchases)as store,
+ sum(numwebpurchases) as webp from most_Accepted ma
+ left join marketing_data  md on md.id = ma.id
+where Recency <=30 
+
+with cte as (
+select * from marketing_data 
+where id not in (select id from most_Accepted))
+
+select avg(income)  from cte
+
+
+create view RFM_WEB as 
+---checking for income greater than the average income
+with highest_income as (
+select * from marketing_data
+where income >
+(select avg(income)  from marketing_data)),--------average income
+
+----checking for recency for the last 30days
+recency as(
+select * from marketing_data
+where recency  <= 30),
+
+---checking RFM
+frequency_webvisit as (--------frequency of webvisited
+select * from marketing_data
+where NumWebVisitsMonth >-----greater than average
+(select avg(numwebvisitsmonth) from marketing_data )),
+
+frequency_storepurhase as (--------------frequency of storepurchase
+select * from marketing_data
+where NumStorePurchases >(---greater than average
+select avg(NumStorePurchases) from marketing_data))
+
+select i.id from highest_income i
+join recency r on i.id = r.id
+join frequency_webvisit f on r.id = f.id
+join frequency_storepurhase s on f.id =s.id
+
+
+create view RFM_store as 
+---checking for income less than the average income
+with highest_income as (
+select * from marketing_data
+where income <
+(select avg(income)  from marketing_data)),--------average income
+
+----checking for recency  for more than 30days 
+recency as(
+select * from marketing_data
+where recency  >= 30),
+
+---checking RFM
+frequency_webvisit as (--------frequency of webvisited
+select * from marketing_data
+where NumWebVisitsMonth <--- less than the average
+(select avg(numwebvisitsmonth) from marketing_data )),
+
+frequency_storepurhase as (--------------frequency of storepurchase
+select * from marketing_data
+where NumStorePurchases <(---less than average
+select avg(NumStorePurchases) from marketing_data))
+
+select i.id from highest_income i
+join recency r on i.id = r.id
+join frequency_webvisit f on r.id = f.id
+join frequency_storepurhase s on f.id =s.id
+
+
+/*checking for behaviour of customers that had income greater than average,
+recency for the last 30days,visted the web greater than the avereage,
+and number of purchase instore greater than average*/
+select * from RFM_WEB Rw
+left join marketing_data md
+ON Rw.id = md.ID
+
+/*checking for behaviour of customers that had income lesser than average,
+recency for mare than 30days,visted the store lesser than the avereage,
+and number of purchase instore lesser than average*/
+select * from RFM_store Rw
+left join marketing_data md
+ON Rw.id = md.ID
+
+
+
+  
+
+select * from marketing_data
+where ID not in (select ID from RFM_WEB) 
+and  ID not in (select id from RFM_WEB2)
+
+
+
+
+select id,min(recency)as min from marketing_data
+group by id
+order by min desc
+
+
+
+
+WITH AggregatedData AS (
+    SELECT 
+        ID,
+        MIN(Recency) AS Recency,
+        SUM(NumStorePurchases + NumWebPurchases + NumCatalogPurchases) AS Frequency,
+        SUM(MntWines + MntMeatProducts + MntFishProducts + MntSweetProducts + MntGoldProds) AS Monetary
+    FROM marketing_data
+    GROUP BY ID
+),
+
+percentile_values AS (
+    SELECT 
+        DISTINCT 
+        PERCENTILE_CONT(0.2) WITHIN GROUP (ORDER BY Monetary) OVER () AS p20_m,
+        PERCENTILE_CONT(0.4) WITHIN GROUP (ORDER BY Monetary) OVER () AS p40_m,
+        PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY Monetary) OVER () AS p60_m,
+        PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY Monetary) OVER () AS p80_m,
+        PERCENTILE_CONT(0.2) WITHIN GROUP (ORDER BY Frequency) OVER () AS p20_f,
+        PERCENTILE_CONT(0.4) WITHIN GROUP (ORDER BY Frequency) OVER () AS p40_f,
+        PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY Frequency) OVER () AS p60_f,
+        PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY Frequency) OVER () AS p80_f,
+        PERCENTILE_CONT(0.2) WITHIN GROUP (ORDER BY Recency) OVER () AS p20_r,
+        PERCENTILE_CONT(0.4) WITHIN GROUP (ORDER BY Recency) OVER () AS p40_r,
+        PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY Recency) OVER () AS p60_r,
+        PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY Recency) OVER () AS p80_r
+       FROM AggregatedData),
+	   
+	   rfm_segments AS (
+SELECT a.ID, a.Recency, a.Frequency, a.Monetary,
+CASE WHEN a.Monetary <= p.p20_m THEN 1
+     WHEN a.Monetary <= p.p40_m THEN 2
+     WHEN a.Monetary <= p.p60_m THEN 3
+     WHEN a.Monetary <= p.p80_m THEN 4
+     ELSE 5 END AS Monetary_Segment,
+	 CASE 
+WHEN a.Frequency <= p.p20_f THEN 1
+WHEN a.Frequency <= p.p40_f THEN 2
+WHEN a.Frequency <= p.p60_f THEN 3
+WHEN a.Frequency <= p.p80_f THEN 4
+ELSE 5
+END AS Frequency_Segment,
+CASE 
+               WHEN a.Recency <= p.p20_r THEN 5
+               WHEN a.Recency <= p.p40_r THEN 4
+               WHEN a.Recency <= p.p60_r THEN 3
+               WHEN a.Recency <= p.p80_r THEN 2
+               ELSE 1
+           END AS Recency_Segment
+    FROM AggregatedData a
+    CROSS JOIN percentile_values p),
+score as (
+	select id,Monetary_Segment+Frequency_Segment+Recency_Segment as score 
+	from rfm_segments),
+customer_segments as (
+select id,
+case  when score >=14 then 'High value customers'
+      when score between 10 and 13 then 'Loyal customers'
+	  else 'Average customers' end as customer_segment
+from score)
+
+select customer_segment,count(c.ID) as cnt_cutomersegment ,count(m.id)cnt_customers,
+(count(m.id)* 100.0) / count(c.id) as percentage
+from customer_segments c
+left join marketing_data m on c.id =m.id
+and  m.AcceptedCmp6 = 1
+group by customer_segment
